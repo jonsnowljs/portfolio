5c5c0743f2941ddc10400d2cc351b4a244dd2e76
Jiasheng Lu <1473875731@qq.com>
2022-02-12 23:55:09 -0700

    add avatar

--- src/components/body/about/About.js
+++ src/components/body/about/About.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import './About.css'
+import avatar from '../../../assets/avatar.svg'
+import SocialContents from '../../common/contacts/SocialContents'
+import Separator from '../../common/separator/Separator';
 
 function About() {
   return (
-    <div className='about'>
-      <div className='about-top'>
-        <div className='about-info'>
+    <div className="about">
+      <div className="about-top">
+        <div className="about-info">
           Hello There, I am
-          <br/> <span className='info-name'>Jiasheng Lu</span>
-          <br/>I'm a full-stack developer.
+          <br /> <span className="info-name">Jiasheng Lu</span>
+          <br />
+          I'm a full-stack developer.
         </div>
-        <div className='about-photo'>
-
+        <div className="about-photo">
+          <img src={avatar} alt="My avatar" className="picture" />
+          
+          
         </div>
       </div>
-      <div className='about-bottom'>
-
-      </div>
+      <SocialContents />
     </div>
-  )
+  );
 }
 
 export default About
