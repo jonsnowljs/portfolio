ab0657e1d9b14ce5c1e8b22c98c297f059ffcf30
Jiasheng Lu <1473875731@qq.com>
2022-02-12 23:56:02 -0700

    create social contents components

---
+++ src/components/common/contacts/SocialContents.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { SocialData } from '../../data/social'
+import './SocialContents.css'
+
+function SocialContents() {
+  const data = SocialData
+  return (
+    <div className='social-contact'>
+      {data.map((item) => {
+        return (
+          <a href={item.link}>
+            <div className='social-icon-div'>
+              <img src={item.icon} className='social-icon' alt={`social_icon_${item.platform}`}/>
+            </div>
+          </a>
+        )
+      })}
+    </div>
+  )
+}
+
+export default SocialContents
