4da6449e439424549688fd67a7e19950bd9958a5
Jiasheng Lu <1473875731@qq.com>
2022-02-12 23:55:45 -0700

    create social data

---
+++ src/components/data/social.js
@@ -0,0 +1,17 @@
+export const SocialData = [
+  {
+    platform: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/jiashenglujob',
+    icon: require('../../assets/linkedin.svg').default,
+  },
+  {
+    platform: 'Github',
+    link: 'https://github.com/jonsnowljs',
+    icon: require('../../assets/github.svg').default,
+  },
+  {
+    platform: 'Gmail',
+    link: 'mailto:jiasheng.lu@edu.sait.ca',
+    icon: require('../../assets/gmail.svg').default,
+  },
+];
