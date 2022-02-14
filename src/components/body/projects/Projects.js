bc696878f524a4e0ad8f1bcaa74ad82b800a1022
Jiasheng Lu <1473875731@qq.com>
2022-02-13 23:31:50 -0700

    Update Projects.js

--- src/components/body/projects/Projects.js
+++ src/components/body/projects/Projects.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Separator from '../../common/separator/Separator';
 import './Projects.css';
+import ProjectTimeline from './ProjectTimeline'
+
+
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <Separator />
+      <ProjectTimeline/>
+
+    </div>
+  );
 }
 
 export default Projects;
