040d9bbf5227ee318d318a1dcd94942bb5eefabe
Jiasheng Lu <1473875731@qq.com>
2022-02-13 23:28:37 -0700

    reorganize image and data file folder

---
+++ src/assets/data/ProjectData.js
@@ -0,0 +1,11 @@
+export const ProjectData = [
+  {
+    id: 1,
+    title: 'My Resume',
+    about: 'This my first resume project. I will show my projects here.',
+    tags: ['React', 'CSS', 'JavaScript'],
+    demo: '',
+    source: 'https://github.com/jonsnowljs/portfolio',
+    img: ''
+  },
+];
--- src/assets/data/SocialData.js
+++ src/assets/data/SocialData.js
@@ -2,16 +2,16 @@ export const SocialData = [
   {
     platform: 'LinkedIn',
     link: 'https://www.linkedin.com/in/jiashenglujob',
-    icon: require('../../assets/linkedin.svg').default,
+    icon: require('../../assets/img/contacts/linkedin.svg').default,
   },
   {
     platform: 'Github',
     link: 'https://github.com/jonsnowljs',
-    icon: require('../../assets/github.svg').default,
+    icon: require('../../assets/img/contacts/github.svg').default,
   },
   {
     platform: 'Gmail',
     link: 'mailto:jiasheng.lu@edu.sait.ca',
-    icon: require('../../assets/gmail.svg').default,
+    icon: require('../../assets/img/contacts/gmail.svg').default,
   },
 ];
