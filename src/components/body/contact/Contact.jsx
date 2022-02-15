83011995254b24e749795e05cd008690b9ac3aae
Jiasheng Lu <1473875731@qq.com>
2022-02-14 23:49:08 -0700

    replace button wiht mui button

--- src/components/body/contact/Contact.jsx
+++ src/components/body/contact/Contact.jsx
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Contact.css';
 import Separator from '../../common/separator/Separator';
 import SocialContacts from '../../common/contacts/SocialContents';
-import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import UseAnimations from 'react-useanimations';
+import download from 'react-useanimations/lib/download';
 
 // Contact section
 function Contact() {
+  const [isPlay, setIsPlay] = useState(true);
+
   return (
     <div className="contact">
       <Separator />
       <label className="section-title">Contact</label>
       <div className="contact-container">
         <div className="contact-left">
-          <p>Contact me </p>
-          <SocialContacts />
-        </div>
-        <div className="download">
-          {/* TODO update resume pdf*/}
+          <div className="download">
+            {/* TODO update resume pdf*/}
 
-          <a download href="../../../assets/resume.pdf">
-            <i class="fi fi-rr-cloud-download download-icon"></i>
-            <Button variant="contained" color="primary">
-              Download Resume
-            </Button>
-          </a>
+            <a download href="../../../assets/resume.pdf">
+              <Button variant="contained" color="info" startIcon={<UseAnimations animation={download} size={"large"} autoPlay={"true"} loop={"true"} />} onClick={() => {setIsPlay(!isPlay)}}>                
+                <span className='button-name'>My Resume</span>
+              </Button>
+            </a>
+          </div>
+          <SocialContacts />
         </div>
       </div>
     </div>
