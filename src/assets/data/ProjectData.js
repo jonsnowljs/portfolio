b7536633a50907a69ddc808c1e80a823a41f5e4f
Jiasheng Lu <1473875731@qq.com>
2022-02-16 00:56:22 -0700

    Update ProjectData.js

--- src/assets/data/ProjectData.js
+++ src/assets/data/ProjectData.js
@@ -3,19 +3,20 @@ export const ProjectData = [
     id: 'project1',
     title: 'My Resume',
     intro:
-      'This my first resume project. I will show myself and my projects here.',
+      'This my first resume project. I will show myself and my projects here. This my first resume project. I will show myself and my projects here.',
     tags: ['React', 'JavaScript', 'CSS', 'MUI'],
     demo: 'https://demo.com',
     github: 'https://github.com/jonsnowljs/portfolio',
-    img: '',
+    img: require('../img/project/demo.jpg').default,
   },
   {
     id: 'project2',
     title: 'My Resume',
-    intro: 'This my first resume project. I will show my projects here.',
+    intro:
+      'This my first resume project. I will show my projects here.This my first resume project. I will show myself and my projects here.',
     tags: ['React', 'CSS', 'JavaScript'],
     demo: '',
     github: 'https://github.com/jonsnowljs/portfolio',
-    img: '',
+    img: require('../img/project/demo.jpg').default,
   },
 ];
