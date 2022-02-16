1a678568bb88d7bfa169ddf9617ce1f874b2f034
Jiasheng Lu <1473875731@qq.com>
2022-02-15 21:05:50 -0700

    change layout to 100vh

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,13 +3,29 @@ import './Home.css';
 import Header from '../header/Header';
 import Body from '../body/Body';
 import Footer from '../footer/Footer';
+import About from '../body/about/About';
+import Separator from '../common/separator/Separator';
+import ProjectTimeline from '../body/projects/ProjectTimeline';
+import Skill from '../body/skills/Skill';
+import Contact from '../body/contact/Contact';
 
 function Home() {
   return (
     <div className="home">
-      <Header/>
-      <Body/>
-      <Footer/>
+      <div className='page-one vh-100'>
+        <Header/>
+        <About/>
+        <Separator/>
+      </div>
+      <div className='page-two'>
+        <ProjectTimeline/>
+      </div>
+      <div className='page-three vh-100'>
+        <Skill/>
+      </div>
+      <div className='page-four vh-100'>
+        <Contact/>
+      </div>
     </div>
   );
 }
