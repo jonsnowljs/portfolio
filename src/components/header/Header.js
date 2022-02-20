d53bcd5720bb13ee8fe2f5fcf94b855a20b58278
Jiasheng Lu <1473875731@qq.com>
2022-02-19 23:42:19 -0700

    use animated  hamburger menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import './Header.css';
 import MobileHeader from './mobile/MobileHeader';
 import WebHeader from './web/WebHeader';
+import { Sling as Hamburger } from 'hamburger-react';
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,8 +14,8 @@ function Header() {
           <div className="web-menu">
             <WebHeader />
           </div>
-          <div onClick={() => {setIsOpen(!isOpen)}} className='mobile-menu'>
-            <i class="fi fi-rr-apps menu-icon" hidden={isOpen}></i>
+          <div onClick={() => {setIsOpen(!isOpen)}} className='mobile-menu close-icon'>
+            <Hamburger toggled={isOpen} toggle={setIsOpen} />
           </div>
 
           {isOpen && <MobileHeader isOpen={isOpen} setIsOpen={setIsOpen}/>}
--- src/components/header/mobile/MobileHeader.js
+++ src/components/header/mobile/MobileHeader.js
@@ -3,9 +3,9 @@ import './MobileHeader.css'
 function MobileHeader({isOpen, setIsOpen}) {
   return (
     <div className="mobile">
-      <div className='close-icon' onClick={()=>{setIsOpen(!isOpen)}}>
+      {/* <div className='close-icon' onClick={()=>{setIsOpen(!isOpen)}}>
         <i class="fi fi-rr-cross-circle"></i>
-      </div>
+      </div> */}
       <div className='mobile-options'>
         <div className="mobile-option">
           <a href="#projects">
