4b3b31549cbbaa974053923e0c6c77085cf87f11
Jiasheng Lu <1473875731@qq.com>
2022-02-20 23:34:59 -0700

    add meta data information

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,11 +7,22 @@ import Skill from '../body/skills/Skill';
 import Contact from '../body/contact/Contact';
 import Projects from '../body/projects/Projects';
 import Title from '../common/title/Title';
+import MetaDecorator from 'components/util/MetaDecorator';
+import { ContentCopy } from '@mui/icons-material';
+import metaThumbnail from '../../assets/img/avatar.svg';
 
+const content = require("../../assets/data/content.json")
 
 function Home() {
+
   return (
     <div className="home">
+      <MetaDecorator
+        description={content}
+        title={content.pageTitle}
+        imageUrl={metaThumbnail}
+        imageAlt={content.metaImageAlt}
+      />
       <div className="page-one vh-100">
         <Header />
         <About />
---
+++ src/components/util/MetaDecorator.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const metaDecorator = require('../../assets/data/metaDecorator.json');
+
+const MetaDecorator = ({ title, description, imageUrl, imageAlt }) => (
+  <Helmet>
+    <title>{title}</title>
+    <meta property="og:title" content={title} />
+    <meta name="description" content={description} />
+    <meta property="og:description" content={description} />
+    {/* TODO add hostname and revise metaDecorator.js data*/}
+    <meta property="og:image" content={metaDecorator.hostname + imageUrl} />
+    <meta
+      property="og:url"
+      content={
+        metaDecorator.hostname +
+        window.location.pathname +
+        window.location.search
+      }
+    />
+    {/* <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:image:alt" content={imageAlt} />
+    <meta name="twitter:site" content={metaDecorator.twitterUsername} /> */}
+  </Helmet>
+);
+
+
+export default MetaDecorator;
