b04211794c3564d3cf0fdd8a4be0fadf7459f7a9
Jiasheng Lu <1473875731@qq.com>
2022-04-03 01:06:46 -0600

    feat: add animation on logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,42 +1,81 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './Header.css';
 import MobileHeader from './mobile/MobileHeader';
 import WebHeader from './web/WebHeader';
 import { Sling as Hamburger } from 'hamburger-react';
+import { useTransition, animated } from '@react-spring/web';
 import useWindowSize from '../common/useWindowSize';
 import useScroll from '../common/useScroll';
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   const size = useWindowSize();
-  const { y, lastY } = useScroll();  
+  const { y, lastY } = useScroll();
 
+  const ref = useRef();
+  const [items, set] = useState([]);
   // close the menu when detect the scroll in vertical direction.
   useEffect(() => {
-    if ( Math.abs(y - lastY) > 10) {
-      setIsOpen(false)
+    if (Math.abs(y - lastY) > 10) {
+      setIsOpen(false);
     }
-  },[y, lastY])
+  }, [y, lastY]);
+  const transitions = useTransition(items, {
+    from: {
+      opacity: 0,
+      height: 0,
+      innerHeight: 0,
+      transform: 'perspective(600px) rotateY(0deg)',
+      color: '#0043d4',
+    },
+    enter: [
+      { opacity: 1, height: 80, innerHeight: 80 },
+      { transform: 'perspective(600px) rotateY(180deg)', color: '#28d79f' },
+      { transform: 'perspective(600px) rotateY(0deg)' },
+    ],
+    leave: [
+      { color: '#rgba(222,236,14,0.918)' },
+      { innerHeight: 0 },
+      { opacity: 0, height: 0 },
+    ],
+    update: { color: '#0043d4' },
+  });
 
+  const reset = useCallback(() => {
+    ref.current?.forEach(clearTimeout);
+    ref.current = [];
+    set([]);
+    ref.current.push(setTimeout(() => set(['Lu', ' Jia', ' Sheng']), 2000));
+    ref.current.push(setTimeout(() => set(['L', 'J', 'S']), 5000));
+    ref.current.push(setTimeout(() => set(['L', '.', 'JS']), 8000));
+  }, []);
+
+  useEffect(() => {
+    reset();
+    return () => ref.current.forEach(clearTimeout);
+  }, []);
 
   return (
     <div className="header">
       <div className="header-box">
-        <div className="logo">L<span>.</span>JS</div>
+        <div className="logo">
+          {transitions(({ innerHeight, ...rest }, item) => (
+            <animated.span style={rest} onClick={reset}>
+              {item}
+            </animated.span>
+          ))}
+        </div>
         <div className="menu">
           <div className="web-menu">
             <WebHeader />
           </div>
-          <div
-            className="mobile-menu close-icon"
-          >
-            {size.width < 720  && (
-              <Hamburger toggled={isOpen} toggle={setIsOpen}/>
+          <div className="mobile-menu close-icon">
+            {size.width < 720 && (
+              <Hamburger toggled={isOpen} toggle={setIsOpen} />
             )}
           </div>
 
-          {(isOpen && size.width < 720) && <MobileHeader isOpen={isOpen} />}
-
+          {isOpen && size.width < 720 && <MobileHeader isOpen={isOpen} />}
         </div>
       </div>
     </div>
