d27b8a527c2b04b7c7d79a5666d3945f9fd71076
Jiasheng Lu <1473875731@qq.com>
2022-04-06 16:57:45 -0600

    chore: add skills

--- src/assets/data/SkillData.js
+++ src/assets/data/SkillData.js
@@ -2,24 +2,115 @@ export const SkillData = [
   {
     type: 'Frontend',
     list: [
-      { name: 'React', icon: require('../../assets/img/skill/react.svg').default },
-      { name: 'JavaScript', icon: require('../../assets/img/skill/javascript.svg').default },
-      { name: 'TypeScript', icon: require('../../assets/img/skill/typescript.svg').default },
-      { name: 'Express.js', icon: require('../../assets/img/skill/express.svg').default },
+      {
+        name: 'React',
+        icon: require('../../assets/img/skill/react.svg').default,
+      },
+      {
+        name: 'Redux',
+        icon: require('../../assets/img/skill/redux.svg').default,
+      },
+      {
+        name: 'JavaScript',
+        icon: require('../../assets/img/skill/javascript.svg').default,
+      },
+      {
+        name: 'TypeScript',
+        icon: require('../../assets/img/skill/typescript.svg').default,
+      },
       { name: 'CSS', icon: require('../../assets/img/skill/css.svg').default },
-      { name: 'HTML', icon: require('../../assets/img/skill/html.svg').default },
-
+      {
+        name: 'Sass',
+        icon: require('../../assets/img/skill/sass.svg').default,
+      },
+      {
+        name: 'Tailwind CSS',
+        icon: require('../../assets/img/skill/tailwindcss.svg').default,
+      },
+      {
+        name: 'MaterialUI',
+        icon: require('../../assets/img/skill/materialui.svg').default,
+      },
+      {
+        name: 'HTML',
+        icon: require('../../assets/img/skill/html.svg').default,
+      },
     ],
   },
   {
     type: 'Backend',
     list: [
-      { name: 'Java', icon: require('../../assets/img/skill/java.svg').default },
-      { name: 'NodeJS', icon: require('../../assets/img/skill/nodejs.svg').default },
-      { name: 'C#', icon: require('../../assets/img/skill/csharp.svg').default },
-      { name: 'python', icon: require('../../assets/img/skill/python.svg').default },
+      {
+        name: 'Java',
+        icon: require('../../assets/img/skill/java.svg').default,
+      },
+      {
+        name: 'Node.js',
+        icon: require('../../assets/img/skill/nodejs.svg').default,
+      },
+      {
+        name: 'Express.js',
+        icon: require('../../assets/img/skill/express.svg').default,
+      },
+      {
+        name: 'C#',
+        icon: require('../../assets/img/skill/csharp.svg').default,
+      },
+      {
+        name: 'python',
+        icon: require('../../assets/img/skill/python.svg').default,
+      },
     ],
   },
+  {
+    type: 'Database and Host platform',
+    list: [
+      {
+        name: 'Oracle SQL',
+        icon: require('../../assets/img/skill/oraclesql.svg').default,
+      },
+      {
+        name: 'MySQL',
+        icon: require('../../assets/img/skill/mysql.svg').default,
+      },
+      {
+        name: 'MongoDB',
+        icon: require('../../assets/img/skill/mongodb.svg').default,
+      },
+      {
+        name: 'Heroku',
+        icon: require('../../assets/img/skill/heroku.svg').default,
+      },
+      {
+        name: 'Github Pages',
+        icon: require('../../assets/img/skill/github.svg').default,
+      },
+      {
+        name: 'Azure',
+        icon: require('../../assets/img/skill/azure.svg').default,
+      },
+      {
+        name: 'Firebase',
+        icon: require('../../assets/img/skill/firebase.svg').default,
+      },
+    ],
+  },
+  {
+    type: 'Test and Project Management',
+    list: [
+      {
+        name: 'Cypress',
+        icon: require('../../assets/img/skill/cypress.svg').default,
+      },
+      {
+        name: 'Jest',
+        icon: require('../../assets/img/skill/jest.svg').default,
+      },
+      { name: 'Git', icon: require('../../assets/img/skill/git.svg').default },
+      {
+        name: 'Jira',
+        icon: require('../../assets/img/skill/jira.svg').default,
+      },
+    ],
+  }
 ];
-
-
