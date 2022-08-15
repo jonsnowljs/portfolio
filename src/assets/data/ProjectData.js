4cf53796b55d49f5db769f4661e4407ceaafe2b4
Jiasheng Lu <1473875731@qq.com>
2022-08-15 03:54:44 -0600

    update new project

--- src/assets/data/ProjectData.js
+++ src/assets/data/ProjectData.js
@@ -1,6 +1,28 @@
 // TODO optimize the image size of all data
 export const ProjectData = [
   {
+    id: 'WhaleSucculent',
+    title: 'Whale Succulent',
+    intro: 'Responsive Full-stack web application for a succulent plant shop. This application is a SAIT capstone project.',
+    tags: [
+      'React',
+      'Express',
+      'NodeJS',
+      'MongoDB',
+      'Redux',
+      'MUI',
+      'GraphQL',
+      'Apollo',
+      'Vercel',
+      'Azure',
+      'Framer Motion',
+      'Github Actions',
+    ],
+    demo: 'https://succulent-frontend.vercel.app/',
+    github: 'https://github.com/WhaleSucculent/succulent_frontend',
+    img: require('../img/project/whalesucculent.png'),
+  },  
+  {
     id: 'project1',
     title: 'My Resume',
     intro:
@@ -27,5 +49,20 @@ export const ProjectData = [
     demo: 'https://shoppingmernappjs.herokuapp.com/',
     github: 'https://github.com/jonsnowljs/Shopping',
     img: require('../img/project/shopping1.png'),
+  },
+  {
+    id: 'project3',
+    title: 'JiraLite(In Progress)',
+    intro:
+      'This is a clone of Jira, written in TypeScript and React. Created debounce with custom hook to decrease the frequency of search request.',
+    tags: [
+      'React',
+      'TypeScript',
+      'React Query',
+      'Jest'
+    ],
+    demo: 'https://jiralite.vercel.app/',
+    github: 'https://github.com/jonsnowljs/jiralite',
+    img: require('../img/project/Jira.png'),
   },
 ];
