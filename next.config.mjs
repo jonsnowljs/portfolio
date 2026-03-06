15af796dbea39d15c1c73f0726064af83252e751
Jiasheng Lu <jonsnowljs@gmail.com>
2026-03-06 04:07:51 -0700

    fix:build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+const isProduction = process.env.NODE_ENV === 'production';
+const repoName = 'portfolio';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: isProduction ? `/${repoName}` : '',
+  assetPrefix: isProduction ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
   },
